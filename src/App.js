e02debcde03c7d73f24be8b84fc25bf1c296f080
Sakib Rahman <seu.sakib@gmail.com>
2022-03-24 15:12:08 +0600

    add header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./component/Header/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/component/Header/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "../../../src/Logo.svg";
+import "./Header.css";
+const Header = () => {
+  return (
+    <nav className="header">
+      <img src={logo} alt="" />
+      <div>
+        <a href="/order">Order</a>
+        <a href="/order-review">Order Review</a>
+        <a href="/manage-inventory">Manage Inventory</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
