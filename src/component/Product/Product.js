6bc78ad7dca3b9c1f52c03dfdf5315d57a2f833b
Sakib Rahman <seu.sakib@gmail.com>
2022-03-25 07:13:30 +0600

    fix product cart bug and product info

--- src/component/Product/Product.js
+++ src/component/Product/Product.js
@@ -1,9 +1,17 @@
 import React from "react";
 import "./Product.css";
 const Product = ({ product }) => {
+  const { img, ratings, name, seller, price } = product;
   return (
-    <div className="container">
-      <img src={product.img} alt="" />
+    <div className="product">
+      {/* console.log({img}); */}
+      <img src={img} alt="" />
+      <div className="product-info">
+        <p>{name}</p>
+        <p>{price}</p>
+        <p>Manufacturer: {seller}</p>
+        <p>Ratings {ratings} stars</p>
+      </div>
     </div>
   );
 };
